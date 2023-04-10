b8d6c4d37e1f409023422ae902054c7eb6f7faf1
Gideon Donkor Bonsu <gideon.bonsu@ashesi.edu.gh>
2023-04-10 20:26:25 +0000

    Generate data for Berekuso and Ketase Highway

--- generate_datapoints_berekuso.py
+++ generate_datapoints_berekuso.py
@@ -2,7 +2,7 @@
 import requests
 import random
 
-FILENAME = "test_features.txt"
+FILENAME = "berekuso_road_datapoints.txt"
 
 
 def post_data_to_api(z_mean, z_variance, z_deviation, z_peak, z_low, latitude, longitude):
@@ -29,14 +29,6 @@ def generate_data(filename):
     with open(filename, "r") as file:
         # Loop through each line in the file
         for line in file:
-            # Sample of random latitude and longitude points for a bad roads- Based on the work of Francis
-            rand_latitude_for_bad = random.uniform(-0.2369879, -0.22195925)
-            rand_longitude_for_bad = random.uniform(5.73995398, 5.76542204)
-
-            # Sample of random latitude and longitude points for a good roads- Based on the work of Francis
-            rand_latitude_for_good = random.uniform(0.19214863, 0.27552346)
-            rand_longitude_for_good = random.uniform(5.6180886, 5.68269272)
-
             # Remove the newline character from the end of the line
             line = line.strip()
 
@@ -49,15 +41,11 @@ def generate_data(filename):
             z_deviation = float(line_items[2])
             z_peak = float(line_items[3])
             z_low = float(line_items[4])
-            grade_of_road = int(line_items[5])
+            rand_latitude = float(line_items[5])
+            rand_longitude = float(line_items[6])
 
-            # If the grade of the road is less than 2, it is bad road, hence, we use the GPS range for the bad roads
-            if grade_of_road <= int(2):
-                print(post_data_to_api(z_mean, z_variance, z_deviation, z_peak, z_low, rand_latitude_for_bad,
-                                       rand_longitude_for_bad))
-            else:
-                print(post_data_to_api(z_mean, z_variance, z_deviation, z_peak, z_low, rand_latitude_for_good,
-                                       rand_longitude_for_good))
+            # Post data to API
+            print(post_data_to_api(z_mean, z_variance, z_deviation, z_peak, z_low, rand_latitude, rand_longitude))
 
 
 generate_data(FILENAME)
